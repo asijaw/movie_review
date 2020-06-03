c5c7e5d8494c2811e0ccd680368f29e41e5bdc64
asijaw <asija.w@yahoo.com>
2020-06-03 01:37:49 -0500

    Fixes error where application was crashing out

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :reviews
-    has_many :movies, through: reviews
+    has_many :movies, through: :reviews
     
     validates :username, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, [:new]
+  resources :users, except: [:new]
   resources :reviews
   resources :movies
 
