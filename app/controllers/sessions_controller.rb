934feac1791329c978caf5bc5966a333f2b89f47
asijaw <asija.w@yahoo.com>
2020-06-03 02:00:44 -0500

    Built out sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,19 @@ class SessionsController < ApplicationController
     def new
         @user = User.new
     end 
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate
+            sessions[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
+    end 
+
+    def destroy 
+        session.delete("user_id")
+        redirect_to root_path
+    end 
 end 
