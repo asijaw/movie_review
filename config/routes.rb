fa25aaeeb086d99729fd9eebd83420c267d34de9
asijaw <asija.w@yahoo.com>
2020-06-03 01:19:59 -0500

    Added some routes and user#new views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, [:new]
   resources :reviews
   resources :movies
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/signup', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
