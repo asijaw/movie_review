dfe76706ad23378ce86c0f8c82a1b2ff4ed0fb2e
asijaw <asija.w@yahoo.com>
2020-06-04 12:25:43 -0500

    added reviews to new movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,13 +14,14 @@ class MoviesController < ApplicationController
     def create 
         @movie = Movie.create(movie_params)
         @user = current_user
+        
         redirect_to movie_path(@movie)
     end
     
     private
 
     def movie_params
-        params.require(:movie).permit(:title, :year, :genre)
+        params.require(:movie).permit(:title, :year, :genre, review_attr: [:content, :rating, :user_id, :movie_id])
     end 
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,13 @@
 class ReviewsController < ApplicationController
     
+    def new 
+        @review = Review.new
+    end 
+
     def create
-        review = Review.create(review_params)
+        @movie = Movie.find(:movie_id)
+        @review = @movie.reviews.build(review_params)
+        @review.user = current_user
     end 
 
     private
