ae41a68053c09e9474363e69fe746843d4bc72bb
asijaw <asija.w@yahoo.com>
2020-06-06 21:30:47 -0500

    fixed sessions logging in bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,19 @@
 class SessionsController < ApplicationController
     def new
-        @user = User.new
+        if logged_in?
+            redirect_to user_path(current_user)
+        else
+            @user = User.new
+        end
     end 
 
     def create
-        @user = User.find_by(username: params[:user][:username])
-        if @user && @user.authenticate(params[:user][:password])
+        @user = User.find_by(username: params[:session][:username])
+        if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id 
             redirect_to @user
         else
+            flash[:alert] = "Invalid entry, please try again"
             render 'new'
         end
     end 
