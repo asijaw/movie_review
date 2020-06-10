e1e5aeb2d33bf1f97e2cc563453ee2b8a46c516e
asijaw <asija.w@yahoo.com>
2020-06-10 09:12:05 -0500

    edited header links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,11 @@ class SessionsController < ApplicationController
     end 
 
     def oauth 
-        @user = User.find_or_create_by(auth) 
+        @user = User.find_or_create_by(uid: auth['uid']) do |u|
+            u.username = auth['info']['name']
+            u.email = auth['info']['email']
+            u.image = auth['info']['image']
+          end
         
         session[:user_id] = @user.id
         redirect_to @user
@@ -31,8 +35,6 @@ class SessionsController < ApplicationController
     end 
 
     private
-    def session_params
-        params.require(:user).permit()
     
     def auth
         request.env['omniauth.auth']
